cd32710e1d035bd92e7fd1b2cff66de190a98202
Neider Hurtatis <neideralejandro.h@gmail.com>
2025-08-05 20:12:00 -0500

    add changes for qr code

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,23 +1,29 @@
 import { lazy, Suspense } from "react";
 import { HashRouter, Navigate, Route } from "react-router";
 import { RoutesNotFound } from "./NotFound";
+import GenerateQr from "@/features/public/qr/GenerateQr";
 
 
 
-const Login = lazy(()=> import('@/features/public/auth/page/Login'))
-const RateProducts = lazy(()=> import('@/features/public/rate-products/page/RateProducts'))
-const RateProductsVoting = lazy(()=> import('@/features/public/rate-products-voting/page/RateProductsVoting'))
+const Login = lazy(() => import('@/features/public/auth/page/Login'))
+const RateProducts = lazy(() => import('@/features/public/rate-products/page/RateProducts'))
+const RateProductsVoting = lazy(() => import('@/features/public/rate-products-voting/page/RateProductsVoting'))
 
-export function AppRouter(){
-  return(
+export function AppRouter() {
+  const qrData = {
+    url: "https://example.com/profile",
+  };
+
+  return (
     <Suspense fallback={<>Cargando.........................</>}>
       <HashRouter>
         <RoutesNotFound>
 
-          <Route path="/" element={<Navigate to="/rate-product" replace/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path='/rate-product' element={<RateProducts/>} />
-          <Route path='/rate-product/:productId' element={<RateProductsVoting/>} />
+          <Route path="/" element={<Navigate to="/rate-product" replace />} />
+          <Route path="/login" element={<Login />} />
+          <Route path='/rate-product' element={<RateProducts />} />
+          <Route path='/rate-product/:productId' element={<RateProductsVoting />} />
+          <Route path='/qr' element={<GenerateQr dataQr={qrData} descriptionShare="Escanea el QR" titleShare="Vota por tu producto favorito"  />} />
         </RoutesNotFound>
       </HashRouter>
 
