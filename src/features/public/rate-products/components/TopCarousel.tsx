145866cf3b51a6b7476524ed9546d8ed970b7c1c
Andres Duque <andress1315@gmail.com>
2025-08-26 18:38:06 -0500

    feat: add carousel

---
+++ src/features/public/rate-products/components/TopCarousel.tsx
@@ -0,0 +1,31 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay  from "embla-carousel-autoplay";
+
+import image_1 from "@/assets/images/image_ads1.webp";
+import image_2 from "@/assets/images/image_ads2.webp";
+
+
+export function TopCarousel() {
+  return (
+    <div className="flex flex-col items-center justify-center  overflow-hidden">
+    <Carousel  className="w-full"  plugins={[Autoplay({ delay: 5000 })]} >
+      <CarouselContent>
+        <CarouselItem>
+          <img src={image_1} alt="image_1" />
+        </CarouselItem>
+        <CarouselItem>
+          <img src={image_2} alt="image_2" />
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-0 top-1/2 -translate-y-1/2" />
+      <CarouselNext className="absolute right-0 top-1/2 -translate-y-1/2" />
+    </Carousel>
+    </div>
+  );
+}
--- src/features/public/rate-products/page/RateProducts.tsx
+++ src/features/public/rate-products/page/RateProducts.tsx
@@ -1,8 +1,10 @@
 import { RateProductGrid } from "../components/RateProductGrid"
+import { TopCarousel } from "../components/TopCarousel"
 
 function RateProducts(){ 
   return(
-    <div className="h-full overflow-auto bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+    <div className="w-full h-full overflow-auto bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+      <TopCarousel/>
       <RateProductGrid/>
     </div>
   )
