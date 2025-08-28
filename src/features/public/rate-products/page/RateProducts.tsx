1870c3e7620b678b0a6ccd038c1b63d2774111eb
Andres Duque <andress1315@gmail.com>
2025-08-27 19:57:25 -0500

    feat: change carousel by lazyimage

--- src/features/public/rate-products/page/RateProducts.tsx
+++ src/features/public/rate-products/page/RateProducts.tsx
@@ -1,13 +1,18 @@
 import { RateProductGrid } from "../components/RateProductGrid"
-import { TopCarousel } from "../components/TopCarousel"
-import { BottomCarousel } from "../components/BottomCarousel"
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import image_2 from "@/assets/images/image_ads2.webp";
+import image_1 from "@/assets/images/image_ads1.webp";
+
 
 function RateProducts(){ 
+  
   return(
     <div className="w-full h-full overflow-auto bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-      <TopCarousel/>
+      
+      <LazyLoadImage src={image_2} alt="Evento" />
       <RateProductGrid/>
-      <BottomCarousel/>
+      
+      <LazyLoadImage src={image_1} alt="Evento" />
     </div>
   )
 }
