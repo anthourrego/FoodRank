a46bf9bdc9d7c350b2baafcd2f04ecb65540852b
Neider Hurtatis <neideralejandro.h@gmail.com>
2025-09-20 11:22:07 -0500

    add chnages for login

---
+++ src/features/public/service/authService.tsx
@@ -0,0 +1,39 @@
+import apiClient from "./axios";
+
+
+const urlBack = import.meta.env.VITE_URL_BACK;
+
+const login = async (email: string, password: string) => {
+  try {
+    const response = await apiClient.post(`${urlBack}auth/login`, {
+      email,
+      password,
+    });
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const logout = async () => {
+  try {
+    const response = await apiClient.post(`${urlBack}auth/logout`);
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getUser = async () => {
+  try {
+    const response = await apiClient.get(`${urlBack}auth/user`);
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export { login, logout, getUser };
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,29 +3,45 @@ import { BrowserRouter, Navigate, Route } from "react-router";
 import { RoutesNotFound } from "./NotFound";
 import GenerateQr from "@/features/public/qr/GenerateQr";
 import { PrivateLayout } from "@/layout/PrivateLayout";
+import AuthProvider from "@/features/public/auth/context/AuthProvider";
+import { Loader2 } from "lucide-react";
 
-
-
-
-const Login = lazy(() => import('@/features/public/auth/page/Login'))
-const RateProducts = lazy(() => import('@/features/public/rate-products/page/RateProducts'))
-const RateProductsVoting = lazy(() => import('@/features/public/rate-products-voting/page/RateProductsVoting'))
+const Login = lazy(() => import("@/features/public/auth/page/Login"));
+const RateProducts = lazy(
+  () => import("@/features/public/rate-products/page/RateProducts")
+);
+const RateProductsVoting = lazy(
+  () => import("@/features/public/rate-products-voting/page/RateProductsVoting")
+);
 
 export function AppRouter() {
   return (
-    <Suspense fallback={<>Cargando.........................</>}>
-      <BrowserRouter>
-        <RoutesNotFound>
-          <Route element={<PrivateLayout/>}>
-            <Route path="/" element={<Navigate to="/rate-product" replace/>} />
-            <Route path="/login" element={<Login/>} />
-            <Route path='/rate-product' element={<RateProducts/>} />
-            <Route path='/rate-product/:productId' element={<RateProductsVoting/>} />
-            <Route path='/qr' element={<GenerateQr />} />
-          </Route>
-        </RoutesNotFound>
-      </BrowserRouter>
-      
+    <Suspense
+      fallback={
+        <div className="min-h-screen flex items-center justify-center">
+          <Loader2 className="animate-spin w-12 h-12 text-red-800" />
+        </div>
+      }
+    >
+      <AuthProvider>
+        <BrowserRouter>
+          <RoutesNotFound>
+            <Route path="/login" element={<Login />} />
+            <Route element={<PrivateLayout />}>
+              <Route
+                path="/"
+                element={<Navigate to="/rate-product" replace />}
+              />
+              <Route path="/rate-product" element={<RateProducts />} />
+              <Route
+                path="/rate-product/:productId"
+                element={<RateProductsVoting />}
+              />
+              <Route path="/qr" element={<GenerateQr />} />
+            </Route>
+          </RoutesNotFound>
+        </BrowserRouter>
+      </AuthProvider>
     </Suspense>
-  )
+  );
 }
